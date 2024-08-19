a2335573f138462efa27599392c521d070ea3fc4
Macchiato <simppp@gmail.com>
2024-08-19 20:55:44 +0900

    다국어 기능 추가

--- Assets/Editor/TextureReplacerEditor.cs
+++ Assets/Editor/TextureReplacerEditor.cs
@@ -18,6 +18,9 @@ namespace com.vrsuya.materialoptimizer {
 		SerializedProperty SerializedAvatarGameObject;
 		SerializedProperty SerializedAvatarMaterials;
 
+		public static int LanguageIndex = 0;
+		public readonly string[] LanguageType = new[] { "English", "한국어", "日本語" };
+
 		void OnEnable() {
 			SerializedAvatarTextureList = serializedObject.FindProperty("AvatarTextureList");
 
@@ -26,57 +29,88 @@ namespace com.vrsuya.materialoptimizer {
 		}
 
         public override void OnInspectorGUI() {
+			GUIStyle CenteredStyle = new GUIStyle(EditorStyles.label) { alignment = TextAnchor.MiddleCenter };
+			GUILayoutOption PropertyWidth = GUILayout.Width((EditorGUIUtility.currentViewWidth - 15f - 50f - 30f) / 2f);
+			GUILayoutOption ButtonWidth = GUILayout.Width(50);
+			GUILayoutOption ArrowWidth = GUILayout.Width(15);
 			TextureReplacer newTextureReplacer = (TextureReplacer)target;
 			serializedObject.Update();
-			EditorGUILayout.PropertyField(SerializedAvatarGameObject, new GUIContent("대상 오브젝트"));
-			EditorGUILayout.PropertyField(SerializedAvatarMaterials, new GUIContent("아바타 머테리얼"));
-			EditorGUILayout.LabelField("변경할 텍스쳐", EditorStyles.boldLabel);
-			for (int Index = 0; Index < SerializedAvatarTextureList.arraySize; Index++) {
-				SerializedProperty TextureProperty = SerializedAvatarTextureList.GetArrayElementAtIndex(Index);
-				SerializedProperty ShowProperty = TextureProperty.FindPropertyRelative("ShowDetails");
-				SerializedProperty BeforeProperty = TextureProperty.FindPropertyRelative("BeforeTexture");
-				SerializedProperty AfterProperty = TextureProperty.FindPropertyRelative("AfterTexture");
-				SerializedProperty MaterialProperty = TextureProperty.FindPropertyRelative("OriginMaterial");
-				bool ShowDetailValue = ShowProperty.boolValue;
-				EditorGUILayout.BeginHorizontal();
-				EditorGUILayout.PropertyField(BeforeProperty, new GUIContent(string.Empty));
-				EditorGUILayout.LabelField("▶", GUILayout.Width(15));
-				EditorGUILayout.PropertyField(AfterProperty, new GUIContent(string.Empty));
-				if (GUILayout.Button(ShowDetailValue ? "숨기기" : "보이기", GUILayout.Width(50))) {
-					ShowProperty.boolValue = !ShowDetailValue;
-				}
-				EditorGUILayout.EndHorizontal();
-				if (ShowProperty.boolValue) {
-					for (int MaterialIndex = 0; MaterialIndex < MaterialProperty.arraySize; MaterialIndex++) {
-						EditorGUI.indentLevel++;
-						EditorGUILayout.BeginHorizontal();
-						SerializedProperty OriginMaterialProperty = MaterialProperty.GetArrayElementAtIndex(MaterialIndex);
-						SerializedProperty OriginMaterial = OriginMaterialProperty.FindPropertyRelative("OriginMaterial");
-						SerializedProperty OriginProperty = OriginMaterialProperty.FindPropertyRelative("PropertyName");
-						string[] StringPropertys = new string[OriginProperty.arraySize];
-						for (int PropertyIndex = 0; PropertyIndex < OriginProperty.arraySize; PropertyIndex++) {
-							SerializedProperty StringProperty = OriginProperty.GetArrayElementAtIndex(PropertyIndex);
-							StringPropertys[PropertyIndex] = StringProperty.stringValue;
+			LanguageIndex = EditorGUILayout.Popup(LanguageHelper.GetContextString("String_Language"), LanguageIndex, LanguageType);
+			EditorGUILayout.PropertyField(SerializedAvatarGameObject, new GUIContent(LanguageHelper.GetContextString("String_TargetAvatar")));
+			GUI.enabled = false;
+			EditorGUILayout.PropertyField(SerializedAvatarMaterials, new GUIContent(LanguageHelper.GetContextString("String_TargetMaterial")));
+			GUI.enabled = true;
+			EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_TargetTexture"), EditorStyles.boldLabel);
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_Before"), CenteredStyle, PropertyWidth);
+			EditorGUILayout.LabelField("▶", CenteredStyle, ArrowWidth);
+			EditorGUILayout.LabelField(LanguageHelper.GetContextString("String_After"), CenteredStyle, PropertyWidth);
+			EditorGUILayout.LabelField(string.Empty, ButtonWidth);
+			EditorGUILayout.EndHorizontal();
+			if (SerializedAvatarTextureList.arraySize > 0) {
+				for (int Index = 0; Index < SerializedAvatarTextureList.arraySize; Index++) {
+					SerializedProperty TextureProperty = SerializedAvatarTextureList.GetArrayElementAtIndex(Index);
+					SerializedProperty ShowProperty = TextureProperty.FindPropertyRelative("ShowDetails");
+					SerializedProperty BeforeProperty = TextureProperty.FindPropertyRelative("BeforeTexture");
+					SerializedProperty AfterProperty = TextureProperty.FindPropertyRelative("AfterTexture");
+					SerializedProperty MaterialProperty = TextureProperty.FindPropertyRelative("OriginMaterial");
+					bool ShowDetailValue = ShowProperty.boolValue;
+					EditorGUILayout.BeginHorizontal();
+					GUI.enabled = false;
+					EditorGUILayout.PropertyField(BeforeProperty, GUIContent.none, PropertyWidth);
+					GUI.enabled = true;
+					EditorGUILayout.LabelField("▶", CenteredStyle, ArrowWidth);
+					EditorGUILayout.PropertyField(AfterProperty, GUIContent.none, PropertyWidth);
+					if (GUILayout.Button(ShowDetailValue ? LanguageHelper.GetContextString("String_Hide") : LanguageHelper.GetContextString("String_Show"), ButtonWidth)) {
+						ShowProperty.boolValue = !ShowDetailValue;
+					}
+					EditorGUILayout.EndHorizontal();
+					if (ShowProperty.boolValue) {
+						for (int MaterialIndex = 0; MaterialIndex < MaterialProperty.arraySize; MaterialIndex++) {
+							EditorGUI.indentLevel++;
+							EditorGUILayout.BeginHorizontal();
+							SerializedProperty OriginMaterialProperty = MaterialProperty.GetArrayElementAtIndex(MaterialIndex);
+							SerializedProperty OriginMaterial = OriginMaterialProperty.FindPropertyRelative("OriginMaterial");
+							SerializedProperty OriginProperty = OriginMaterialProperty.FindPropertyRelative("PropertyName");
+							string[] StringPropertys = new string[OriginProperty.arraySize];
+							for (int PropertyIndex = 0; PropertyIndex < OriginProperty.arraySize; PropertyIndex++) {
+								SerializedProperty StringProperty = OriginProperty.GetArrayElementAtIndex(PropertyIndex);
+								StringPropertys[PropertyIndex] = StringProperty.stringValue;
+							}
+							GUI.enabled = false;
+							EditorGUILayout.PropertyField(OriginMaterial, new GUIContent(string.Empty));
+							GUI.enabled = true;
+							EditorGUILayout.LabelField(String.Join(Environment.NewLine, StringPropertys));
+							EditorGUILayout.EndHorizontal();
+							EditorGUI.indentLevel--;
 						}
-						EditorGUILayout.PropertyField(OriginMaterial, new GUIContent(string.Empty));
-						EditorGUILayout.LabelField(String.Join(Environment.NewLine, StringPropertys));
-						EditorGUILayout.EndHorizontal();
-						EditorGUI.indentLevel--;
 					}
 				}
+			} else {
+				EditorGUILayout.HelpBox(LanguageHelper.GetContextString("NO_DATA"), MessageType.Info);
 			}
-			EditorGUILayout.BeginHorizontal();
-			EditorGUILayout.EndHorizontal();
-			if (GUILayout.Button("아바타 머테리얼 다시 가져오기")) {
+			EditorGUILayout.HelpBox(LanguageHelper.GetContextString("String_Null"), MessageType.Info);
+			if (GUILayout.Button(LanguageHelper.GetContextString("String_Refresh"))) {
 				(target as TextureReplacer).RefreshAvatarProprety();
 				Repaint();
 			}
 			EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 			serializedObject.ApplyModifiedProperties();
-			if (GUILayout.Button("텍스쳐 교체하기")) {
+			if (GUILayout.Button(LanguageHelper.GetContextString("String_Replace"))) {
 				(target as TextureReplacer).ChangeAvatarTextures();
+				(target as TextureReplacer).RefreshAvatarProprety();
 				Repaint();
 			}
+			EditorGUILayout.BeginHorizontal();
+			if (GUILayout.Button(LanguageHelper.GetContextString("String_Undo"))) {
+				Undo.PerformUndo();
+				(target as TextureReplacer).RefreshAvatarProprety();
+				Repaint();
+			}
+			if (GUILayout.Button(LanguageHelper.GetContextString("String_Save"))) {
+				AssetDatabase.SaveAssets();
+			}
+			EditorGUILayout.EndHorizontal();
 		}
     }
 }
---
+++ Assets/Editor/TextureReplacerEditor_Language.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+
+/*
+ * VRSuya VRSuya MaterialOptimizer TextureReplacer Editor
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.materialoptimizer {
+
+	public class LanguageHelper : TextureReplacerEditor {
+
+		/// <summary>요청한 값을 설정된 언어에 맞춰 값을 반환합니다.</summary>
+		/// <returns>요청한 String의 현재 설정된 언어 버전</returns>
+		internal static string GetContextString(string RequestContext) {
+			string ReturnContext = RequestContext;
+			switch (LanguageIndex) {
+				case 0:
+					if (String_English.ContainsKey(RequestContext)) {
+						ReturnContext = String_English[RequestContext];
+					}
+					break;
+				case 1:
+					if (String_Korean.ContainsKey(RequestContext)) {
+						ReturnContext = String_Korean[RequestContext];
+					}
+					break;
+				case 2:
+					if (String_Japanese.ContainsKey(RequestContext)) {
+						ReturnContext = String_Japanese[RequestContext];
+					}
+					break;
+			}
+			return ReturnContext;
+		}
+
+		// 영어 사전 데이터
+		private static Dictionary<string, string> String_English = new Dictionary<string, string>() {
+			{ "String_Language", "Language" },
+			{ "String_TargetAvatar", "Target Avatar" },
+			{ "String_TargetMaterial", "Materials in use" },
+			{ "String_TargetTexture", "Textures in use" },
+			{ "String_Before", "Existing Textures" },
+			{ "String_After", "Textures to Change" },
+			{ "String_Show", "Show" },
+			{ "String_Hide", "Hide" },
+			{ "String_Refresh", "Refresh" },
+			{ "String_Replace", "Replace Texture" },
+			{ "String_Undo", "Undo" },
+			{ "String_Save", "Save" },
+			{ "String_Null", "Clearing the item will remove the texture from the material" },
+
+			{ "NO_DATA", "The texture cannot be found in the specified object" }
+		};
+
+		// 한국어 사전 데이터
+		private static Dictionary<string, string> String_Korean = new Dictionary<string, string>() {
+			{ "String_Language", "언어" },
+			{ "String_TargetAvatar", "대상 아바타" },
+			{ "String_TargetMaterial", "사용 중인 머테리얼" },
+			{ "String_TargetTexture", "사용 중인 텍스쳐" },
+			{ "String_Before", "기존 텍스쳐" },
+			{ "String_After", "변경할 텍스쳐" },
+			{ "String_Show", "표시" },
+			{ "String_Hide", "숨기기" },
+			{ "String_Refresh", "새로 고침" },
+			{ "String_Replace", "텍스쳐 교체" },
+			{ "String_Undo", "실행 취소" },
+			{ "String_Save", "저장" },
+			{ "String_Null", "항목을 비우면 해당 텍스쳐를 머테리얼에서 제거합니다" },
+
+			{ "NO_DATA", "해당 오브젝트에서 텍스쳐를 찾을 수 없습니다" }
+		};
+
+		// 일본어 사전 데이터
+		private static Dictionary<string, string> String_Japanese = new Dictionary<string, string>() {
+			{ "String_Language", "言語" },
+			{ "String_TargetAvatar", "対象アバター" },
+			{ "String_TargetMaterial", "使用中のマテリアル" },
+			{ "String_TargetTexture", "使用中のテクスチャ" },
+			{ "String_Before", "既存のテクスチャ" },
+			{ "String_After", "変更するテクスチャ" },
+			{ "String_Show", "表示" },
+			{ "String_Hide", "非表示" },
+			{ "String_Refresh", "更新" },
+			{ "String_Replace", "テクスチャ交換" },
+			{ "String_Undo", "元に戻す" },
+			{ "String_Save", "保存" },
+			{ "String_Null", "項目をクリアすると、該当テクスチャがマテリアルから削除されます" },
+
+			{ "NO_DATA", "該当オブジェクトでテクスチャを見つけることができません" }
+		};
+	}
+}
